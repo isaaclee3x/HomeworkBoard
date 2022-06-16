26a6bcf5083f0859596714df642b76fec0b462b8
isaac <baa44422@gmail.com>
2022-06-16 21:05:52 +0800

    Initial Commit

---
+++ HomeworkBoardNew/HomeworkBoardNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeworkBoardNewApp.swift
+//  HomeworkBoardNew
+//
+//  Created by Isaac Lee Jing Zhi on 16/6/22.
+//
+
+import SwiftUI
+
+@main
+struct HomeworkBoardNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
