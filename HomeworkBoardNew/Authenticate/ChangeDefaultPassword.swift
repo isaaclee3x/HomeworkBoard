5238111f9eb267b807d051f90854514d6a80f3e3
Isaac <baa44422@gmail.com>
2022-07-25 12:43:19 +0800

    Making a new view for user to change their default password

---
+++ HomeworkBoardNew/Authenticate/ChangeDefaultPassword.swift
@@ -0,0 +1,20 @@
+//
+//  ChangeDefaultPassword.swift
+//  HomeworkBoardNew
+//
+//  Created by Isaac Lee Jing Zhi on 25/7/22.
+//
+
+import SwiftUI
+
+struct ChangeDefaultPassword: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChangeDefaultPassword_Previews: PreviewProvider {
+    static var previews: some View {
+        ChangeDefaultPassword()
+    }
+}
