215a20d3b1bd8aa503635c9b88b3c71e04ad25b4
isaaclee <75001370+isaaclee3x@users.noreply.github.com>
2022-12-16 14:56:44 +0800

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HomeworkBoard
+//
+//  Created by Isaac Lee Jing Zhi on 16/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HomeworkBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeworkBoardApp.swift
+//  HomeworkBoard
+//
+//  Created by Isaac Lee Jing Zhi on 16/12/22.
+//
+
+import SwiftUI
+
+@main
+struct HomeworkBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
